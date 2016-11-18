008143c9bd23495c61f2d407ebe72e88a28bae25
datovard <datovard@unal.edu.co>
2016-11-18 17:02:49 -0500

    Added more expression functionalities

--- src/MyVisitor.java
+++ src/MyVisitor.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 public class MyVisitor<T> extends PHPParserBaseVisitor<T> {
 
     private PrintWriter writer;
+    private String tabulations;
 
     public MyVisitor( String file ){
         writer = null;
@@ -12,6 +13,7 @@ public class MyVisitor<T> extends PHPParserBaseVisitor<T> {
         } catch (Exception e) {
             // do something
         }
+        tabulations = "";
     }
 
     public void closeFile(){
@@ -25,35 +27,133 @@ public class MyVisitor<T> extends PHPParserBaseVisitor<T> {
 
     @Override
     public T visitTopStatement(PHPParser.TopStatementContext ctx) {
-        return super.visitTopStatement(ctx);
+        String val = (String) super.visitTopStatement(ctx);
+        writer.println(val+"");
+        return null;
+    }
+
+    @Override
+    public T visitInnerStatementList(PHPParser.InnerStatementListContext ctx) {
+        String total = "";
+        for (PHPParser.InnerStatementContext ct: ctx.innerStatement()){
+            String val = (String) visitInnerStatement(ct);
+            total += tabulations+val+"\n";
+        }
+        return (T)( total );
+    }
+
+    @Override
+    public T visitInnerStatement(PHPParser.InnerStatementContext ctx) {
+        return super.visitInnerStatement(ctx);
+    }
+
+    @Override
+    public T visitStatement(PHPParser.StatementContext ctx) {
+        return super.visitStatement(ctx);
+    }
+
+    @Override
+    public T visitNonEmptyStatement(PHPParser.NonEmptyStatementContext ctx) {
+        return super.visitNonEmptyStatement(ctx);
+    }
+
+    @Override
+    public T visitBlockStatement(PHPParser.BlockStatementContext ctx) {
+        tabulations += "\t";
+        String inner = (String) visitInnerStatementList(ctx.innerStatementList());
+        tabulations = tabulations.substring(0, tabulations.length()-1);
+        return (T)("{\n"+inner+tabulations+"}");
+    }
+
+    @Override
+    public T visitIfStatement(PHPParser.IfStatementContext ctx) {
+        String total = "", elseIfStr = "", elseStr="";
+        String paren = (String) visitParenthesis(ctx.parenthesis());
+        if( ctx.statement() != null ) {
+            String state = (String) visitStatement(ctx.statement());
+            total = "if" + paren + state;
+            if (ctx.elseIfStatement() != null) {
+                List<PHPParser.ElseIfStatementContext> elses = ctx.elseIfStatement();
+                for (PHPParser.ElseIfStatementContext ct : elses) {
+                    String elseval = (String) visitElseIfStatement(ct);
+                    elseIfStr += elseval;
+                }
+            }
+            if (ctx.elseStatement() != null) {
+                elseStr = (String) visitElseStatement(ctx.elseStatement());
+            }
+            total += elseIfStr + elseStr;
+        }else{
+            tabulations += "\t";
+            String inner = (String)visitInnerStatementList(ctx.innerStatementList());
+            tabulations = tabulations.substring(0, tabulations.length()-1);
+            total = "if" + paren + "{\n"+inner+"}";
+            if(ctx.elseIfColonStatement() != null){
+                List<PHPParser.ElseIfColonStatementContext> elses = ctx.elseIfColonStatement();
+                for(PHPParser.ElseIfColonStatementContext ct: elses){
+                    tabulations += "\t";
+                    String elseval = (String) visitElseIfColonStatement(ct);
+                    tabulations = tabulations.substring(0, tabulations.length()-1);
+                    elseIfStr += elseval;
+                }
+            }
+            if( ctx.elseColonStatement() != null ){
+                elseStr += (String) visitElseColonStatement(ctx.elseColonStatement());
+            }
+            total += elseIfStr + elseStr;
+        }
+        return (T)total;
+    }
+
+    @Override
+    public T visitElseIfStatement(PHPParser.ElseIfStatementContext ctx) {
+        String total = "", elseStr = "";
+        String paren = (String) visitParenthesis(ctx.parenthesis());
+        String state = (String) visitStatement(ctx.statement());
+        total = "else if"+paren+state;
+        return (T) total;
+    }
+
+    @Override
+    public T visitElseIfColonStatement(PHPParser.ElseIfColonStatementContext ctx) {
+        String total = "", elseStr = "";
+        String paren = (String) visitParenthesis(ctx.parenthesis());
+        String inner = (String) visitInnerStatementList( ctx.innerStatementList() );
+        total = "else if"+paren+"{\n"+tabulations+inner+"}";
+        return (T) total;
+    }
+
+    @Override
+    public T visitElseStatement(PHPParser.ElseStatementContext ctx) {
+        String total = "";
+        String state = (String) visitStatement(ctx.statement());
+        total = "else"+state;
+        return (T) total;
+    }
+
+    @Override
+    public T visitElseColonStatement(PHPParser.ElseColonStatementContext ctx) {
+        String total = "";
+        String inner = (String) visitInnerStatementList(ctx.innerStatementList());
+        total = "else"+inner;
+        return (T) total;
     }
 
     @Override
     public T visitEchoStatement(PHPParser.EchoStatementContext ctx) {
         ArrayList<String> values = (ArrayList<String>) visitExpressionList(ctx.expressionList());
+        String total = "";
         for(String str : values) {
-            writer.println("console.log(" + str + ");");
+            total += "response.write(" + str + ");";
+            if( values.size() > 1 ) total += "\n";
         }
-        return null;
+        return (T)total;
     }
 
     @Override
-    public T visitExpressionList(PHPParser.ExpressionListContext ctx) {
-        List<String> expressions = new ArrayList<String>();
-        for(PHPParser.ExpressionContext ct : ctx.expression()){
-            String resp = (String) visitExpression(ct);
-            expressions.add(resp);
-        }
-        return (T) expressions;
-    }
-
-    @Override
-    public T visitParenthesis(PHPParser.ParenthesisContext ctx) {
-        if( ctx.expression() != null){
-            String val = (String) visitExpression(ctx.expression());
-            return (T)( "("+val+")" );
-        }
-        return visitYieldExpression(ctx.yieldExpression());
+    public T visitExpressionStatement(PHPParser.ExpressionStatementContext ctx) {
+        String val = (String) visitExpression(ctx.expression());
+        return (T)(val+";");
     }
 
     @Override
@@ -74,6 +174,25 @@ public class MyVisitor<T> extends PHPParserBaseVisitor<T> {
         if( ctx.getChild(1).toString().equals("xor") )
             value = valexp + " ^ " + valandor;
         return (T) value;
+    }
+
+    @Override
+    public T visitExpressionList(PHPParser.ExpressionListContext ctx) {
+        List<String> expressions = new ArrayList<String>();
+        for(PHPParser.ExpressionContext ct : ctx.expression()){
+            String resp = (String) visitExpression(ct);
+            expressions.add(resp);
+        }
+        return (T) expressions;
+    }
+
+    @Override
+    public T visitParenthesis(PHPParser.ParenthesisContext ctx) {
+        if( ctx.expression() != null){
+            String val = (String) visitExpression(ctx.expression());
+            return (T)( "( "+val+" )" );
+        }
+        return visitYieldExpression(ctx.yieldExpression());
     }
 
     @Override
@@ -121,12 +240,46 @@ public class MyVisitor<T> extends PHPParserBaseVisitor<T> {
 
     @Override
     public T visitMultiplicationExpression(PHPParser.MultiplicationExpressionContext ctx) {
-
+        if( ctx.multiplicationExpression() != null ){
+            if( ctx.typeRef() != null ){
+                return null;
+            }else{
+                String valmul = (String) visitMultiplicationExpression(ctx.multiplicationExpression());
+                String valnot = (String) visit( ctx.notLeftRecursionExpression() );
+                if( ctx.getChild(1).getText().equals("**") )
+                    return (T) (" Math.pow("+valnot+","+valmul+") ");
+                else if( ctx.getChild(1).getText().equals("*") || ctx.getChild(1).getText().equals("/") || ctx.getChild(1).getText().equals("%") )
+                    return (T) (valmul+" "+ctx.getChild(1).getText()+" "+valnot);
+            }
+        }
         return super.visitMultiplicationExpression(ctx);
     }
 
     @Override
-    public T visitString(PHPParser.StringContext ctx) {
-        return super.visitString(ctx);
+    public T visitAssignmentExpression(PHPParser.AssignmentExpressionContext ctx) {
+        List<PHPParser.ChainContext> chains = (List<PHPParser.ChainContext>) ctx.chain();
+        if( chains.size() != 1 ){
+
+        }else{
+            String chn = (String) visitChain(chains.get(0));
+            String exp = (String) visitExpression(ctx.expression());
+            return (T)( "var "+chn+" "+ctx.assignmentOperator().getText()+" "+exp);
+        }
+        return super.visitAssignmentExpression(ctx);
     }
+
+    @Override
+    public T visitChain(PHPParser.ChainContext ctx) { return (T)( ctx.getText().replace("$","") ); }
+
+    @Override
+    public T visitPrefixIncDecExpression(PHPParser.PrefixIncDecExpressionContext ctx) {
+        String chn = (String) visitChain(ctx.chain());
+        return (T)( ctx.getChild(0)+chn );
+    }
+
+    @Override
+    public T visitConstant(PHPParser.ConstantContext ctx) { return (T) ctx.getText(); }
+
+    @Override
+    public T visitString(PHPParser.StringContext ctx) { return (T) ctx.getText(); }
 }
